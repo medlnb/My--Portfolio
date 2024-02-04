91b5e5413b9a66c195b76624b496dd7c8c920285
TWSU <mohamedlanabi0@gmail.com>
2024-02-04 23:52:21 +0100

    adding resume

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { FaFigma } from "react-icons/fa";
 import "./Nav.css";
 
 function Navbar() {
   return (
-    <header className="headers">
-      <nav className="navbar">
-        <NavLink to="/" className="notactive_navbar_element">
-          _hello
-        </NavLink>
-        <NavLink to="/about_me" className="notactive_navbar_element">
-          _about me
-        </NavLink>
-        <NavLink to="/projects" className="notactive_navbar_element">
-          _projects
-        </NavLink>
-        <NavLink to="/skills" className="notactive_navbar_element">
-          _Skills
-        </NavLink>
-      </nav>
-    </header>
+    <>
+      <header className="headers">
+        <nav className="navbar">
+          <NavLink to="/" className="notactive_navbar_element">
+            _hello
+          </NavLink>
+          <NavLink to="/about_me" className="notactive_navbar_element">
+            _about me
+          </NavLink>
+          <NavLink to="/projects" className="notactive_navbar_element">
+            _projects
+          </NavLink>
+          <NavLink to="/skills" className="notactive_navbar_element">
+            _Skills
+          </NavLink>
+        </nav>
+        <a
+          target="_blank"
+          href="https://www.figma.com/file/kM6G8u4jp614eaqWovQ3wS/Resume-(Copy)?type=design&node-id=0%3A1&mode=design&t=0YMghWM1ZyDr2ykN-1"
+          className="resume--link"
+        >
+          <FaFigma /> resume.get()
+        </a>
+      </header>
+    </>
   );
 }
 
